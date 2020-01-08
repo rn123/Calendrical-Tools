e4af8f590ecc6072c047dc66b40107494a35bfb3
rn123 <ray.nowell@gmail.com>
2020-01-08 13:42:58 -1000

    Revert "try the silly comment again"
    This reverts commit 3393bbd11c6e2d80dbad8271144157498c829f1b.

--- display_candybars.py
+++ display_candybars.py
@@ -118,8 +118,6 @@ for w in gregorian_weeks:
         output += ('')
     formatted_weeks.append(output)
 
-print('Oh no!')
-
 lunar_template_text = r'''\begin{tabular}{c}
 {% for w in weeks -%}
     {{ w }} \\
