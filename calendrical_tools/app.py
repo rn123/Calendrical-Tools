d24854ac714e32796d6e0a5534ea3b08b02ef795
rn123 <ray.nowell@gmail.com>
2020-02-04 09:13:24 -1000

    setup environment and minimal example for IEX data api

--- calendrical_tools/app.py
+++ calendrical_tools/app.py
@@ -2,11 +2,24 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-app = dash.Dash()
+# iexfinance example data, sign up and use free sandbox api, get token
+# Save token to IEX_TOKEN, specify sandbox API 
+# with export IEX_API_VERSION='iexcloud-sandbox'
+from iexfinance.stocks import get_historical_data
+import datetime
+from dateutil.relativedelta import relativedelta
 
-app.layout = html.Div(
-	html.H1(children="Hello World!")
-)
+start = datetime.datetime.today() - relativedelta(years=5)
+end = datetime.datetime.today()
 
-if __name__ == "__main__":
-	app.run_server(debug=True)
+df = get_historical_data("GE", start=start, end=end, output_format="pandas")
+print(df.head())
+
+# app = dash.Dash()
+
+# app.layout = html.Div(
+# 	html.H1(children="Hello World!")
+# )
+
+# if __name__ == "__main__":
+# 	app.run_server(debug=True)
