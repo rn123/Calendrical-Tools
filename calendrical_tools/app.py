8310d25ee24b68fa1fd284211362236102e6c749
rn123 <ray.nowell@gmail.com>
2020-02-04 09:23:58 -1000

    minimal app to chart IEX data api

--- calendrical_tools/app.py
+++ calendrical_tools/app.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
 
 # iexfinance example data, sign up and use free sandbox api, get token
 # Save token to IEX_TOKEN, specify sandbox API 
@@ -13,13 +14,29 @@ start = datetime.datetime.today() - relativedelta(years=5)
 end = datetime.datetime.today()
 
 df = get_historical_data("GE", start=start, end=end, output_format="pandas")
-print(df.head())
 
-# app = dash.Dash()
+trace_close = go.Scatter(x=list(df.index),
+	                     y=list(df.close),
+	                     name="Close",
+	                     line=dict(color="#ebfe00"))
 
-# app.layout = html.Div(
-# 	html.H1(children="Hello World!")
-# )
+data = [trace_close]
 
-# if __name__ == "__main__":
-# 	app.run_server(debug=True)
+layout = dict(title="Stock Chart",
+			  showlegend=False)
+
+fig = dict(data=data, layout=layout)
+
+app = dash.Dash()
+
+app.layout = html.Div([
+	html.Div(html.H1(children="Hello World!")),
+	html.Label("DASH GRAPH"),
+	html.Div(
+		dcc.Graph(id="Stock Chart",
+			      figure=fig)
+	)
+])
+
+if __name__ == "__main__":
+	app.run_server(debug=True)
